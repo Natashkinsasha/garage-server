1fa8458d7f04d272fd23b72d39da1de0c9723bb3
alex <natashkinsasha@gmail.com>
2017-07-08 00:06:14 +0300

    fix JWTService

--- src/service/JWTService.js
+++ src/service/JWTService.js
@@ -2,33 +2,36 @@ import shortId from 'shortid';
 import jwt from 'jsonwebtoken';
 import Promise from 'bluebird';
 
-function JWTService({ redisClient, secret: globalSecret }) {
+function JWTService({redisClient, secret: globalSecret}) {
 
 
-    this.createToken = ({ data = {}, type = 'ACCESS', iss = 'garage', ...claims }, secret) => {
-        const id = data.id || ``;
-        const jti = `${shortId.generate()}-${id}`;
-        return Promise.promisify(jwt.sign)({ data, jti, type, iss, ...claims  }, secret || globalSecret)
-            .then((token) => Promise.promisify(jwt.verify)(token, secret)
-                .then((decoded) => (redisClient.hmset(`${jti}:${type}:${iss}`, 'jti', jti, 'type', type, 'iss', iss, 'iat', decoded.iat, 'data', JSON.stringify(data)))
+    this.createToken = ({data = {}, type = 'ACCESS', iss = 'garage', id, ...claims}, secret) => {
+        const jti = `${shortId.generate()}-${id || data.id || ''}`;
+        return Promise.promisify(jwt.sign)({data, jti, type, iss, ...claims}, secret || globalSecret)
+            .then((token) => Promise.promisify(jwt.verify)(token, secret || globalSecret)
+                .then((decoded) => (redisClient
+                    .hmset(`${jti}:${type}:${iss}`, {
+                        ...decoded,
+                        data: JSON.stringify(data)
+                    }))
                     .return(token)
                 )
             )
     };
 
-    this.createAccessToken = ({ data, iss = 'garage' }, secret) => {
-        return this.createToken({ data, type: 'ACCESS', iss }, secret);
+    this.createAccessToken = ({data, iss = 'garage'}, secret) => {
+        return this.createToken({data, type: 'ACCESS', iss}, secret);
     };
 
-    this.createRefreshToken = ({ data, iss = 'garage' }, secret) => {
-        return this.createToken({ data, type: 'REFRESH', iss }, secret);
+    this.createRefreshToken = ({data, iss = 'garage'}, secret) => {
+        return this.createToken({data, type: 'REFRESH', iss}, secret);
     };
 
-    this.createTokens = ({ data, iss = `garage` }, secret) => {
+    this.createTokens = ({data, iss = `garage`}, secret) => {
         return Promise
             .props({
-                accessToken: this.createAccessToken({ data, iss }, secret),
-                refreshToken: this.createRefreshToken({ data, iss }, secret),
+                accessToken: this.createAccessToken({data, iss}, secret),
+                refreshToken: this.createRefreshToken({data, iss}, secret),
             });
     };
 
@@ -39,87 +42,65 @@ function JWTService({ redisClient, secret: globalSecret }) {
 
     this.isActualAccessToken = (payloadOrToken, secret) => {
         return getPayload(payloadOrToken, secret)
-            .then(({ jti, iss, type })=>{
-                if(type!=='ACCESS'){
+            .then(({jti, iss, type}) => {
+                if (type !== 'ACCESS') {
                     return false;
                 }
-                return isActual({ jti, iss, type });
+                return isActual({jti, iss, type});
             })
     };
 
     this.isActualRefreshToken = (payloadOrToken, secret) => {
         return getPayload(payloadOrToken, secret)
-            .then(({ jti, iss, type })=>{
-                if(type!=='REFRESH'){
+            .then(({jti, iss, type}) => {
+                if (type !== 'REFRESH') {
                     return false;
                 }
-                return isActual({ jti, iss, type });
+                return isActual({jti, iss, type});
             })
     };
 
-    function getPayload(payloadOrToken, secret) {
-        return Promise
-            .resolve()
-            .then(() => {
-                if (payloadOrToken instanceof Object) {
-                    return payloadOrToken;
-                }
-                return Promise.promisify(jwt.verify)(payloadOrToken, secret || globalSecret);
-            })
-    }
 
-    function isActual({ jti, iss, type }) {
-        return Promise
-            .resolve({ jti, iss, type })
-            .then(({ jti, iss, type }) => {
-                if (!jti || !iss || !type) {
-                    return redisClient
-                        .keys(`${jti || '*'}:${type || '*'}:${iss || '*'}`)
-                        .get(0)
-                        .then(Boolean);
+    this.breakToken = (payloadOrToken, secret) => {
+        return getPayload(payloadOrToken, secret)
+            .then(({jti, iss, type, id}) => (breakToken({jti, iss, type, id})));
+    };
+
+
+    this.breakRefreshToken = ({jti, iss, id}) => {
+        return getPayload(payloadOrToken, secret)
+            .then(({jti, iss, type, id}) => {
+                if (type !== `REFRESH`) {
+                    return 0;
                 }
-                return redisClient
-                    .get(`${jti}:${type}:${iss}`)
-                    .then(Boolean);
+                return breakToken({jti, iss, type, id})
             });
-    }
-
-
-    this.breakToken = ({ jti, iss, type, id }) => {
-        if (!jti || !iss || !type) {
-            const key = id && `*${id || '*'}:${type || '*'}:${iss || '*'}` || `${jti || '*'}:${type || '*'}:${iss || '*'}`;
-            return redisClient
-                .keys(key)
-                .then(redisClient.del);
-        }
-        return redisClient
-            .del(`${jti}:${type}:${iss}`);
     };
 
-    this.breakRefreshToken = ({ jti, iss = `*`, id }) => {
-        return this.breakToken({ jti, iss, type: `REFRESH`, id })
-            .return(id);
+    this.breakAccessToken = ({jti, iss, id}) => {
+        return getPayload(payloadOrToken, secret)
+            .then(({jti, iss, type, id}) => {
+                if (type !== `ACCESS`) {
+                    return 0;
+                }
+                return breakToken({jti, iss, type, id})
+            });
     };
 
-    this.breakAccessToken = ({ jti, iss = `*`, id }) => {
-        return this.breakToken({ jti, iss, type: `ACCESS`, id })
-            .return(id);
-    };
-
-    this.getTokensPayload = ({ jti, iss, type, id }) => {
+    this.getTokensPayload = ({jti, iss, type, id}) => {
         if (!jti || !iss || !type) {
             const key = id && `*${id || '*'}:${type || '*'}:${iss || '*'}` || `${jti || '*'}:${type || '*'}:${iss || '*'}`;
             return redisClient
                 .keys(key)
                 .map(redisClient.hgetall)
-                .map((tokenPayload) => ({ ...tokenPayload, data: JSON.parse(tokenPayload.data) }))
+                .map((tokenPayload) => ({...tokenPayload, data: JSON.parse(tokenPayload.data)}))
         }
         return redisClient
             .hgetall(`${jti}:${type}:${iss}`)
-            .then((tokenPayload) => ({ ...tokenPayload, data: JSON.parse(tokenPayload.data) }));
+            .then((tokenPayload) => ({...tokenPayload, data: JSON.parse(tokenPayload.data)}));
     };
 
-    this.getTokens = ({ jti, iss, type, id }, secret) => {
+    this.getTokens = ({jti, iss, type, id}, secret) => {
         if (!jti || !iss || !type) {
             const key = id && `*${id || '*'}:${type || '*'}:${iss || '*'}` || `${jti || '*'}:${type || '*'}:${iss || '*'}`;
             return redisClient
@@ -137,6 +118,49 @@ function JWTService({ redisClient, secret: globalSecret }) {
                 data: JSON.parse(tokenPaylod.data)
             }, secret || globalSecret)));
     };
+
+
+    function getPayload(payloadOrToken, secret) {
+        return Promise
+            .resolve()
+            .then(() => {
+                if (payloadOrToken instanceof Object) {
+                    return payloadOrToken;
+                }
+                return Promise.promisify(jwt.verify)(payloadOrToken, secret || globalSecret);
+            })
+    }
+
+    function isActual({jti, iss, type}) {
+        return Promise
+            .resolve({jti, iss, type})
+            .then(({jti, iss, type}) => {
+                if (!jti || !iss || !type) {
+                    return redisClient
+                        .keys(`${jti || '*'}:${type || '*'}:${iss || '*'}`)
+                        .get(0)
+                        .then(Boolean);
+                }
+                return redisClient
+                    .get(`${jti}:${type}:${iss}`)
+                    .then(Boolean);
+            });
+    }
+
+    function breakToken({jti, iss, type, id}) {
+        return Promise
+            .resolve({jti, iss, type})
+            .then(({jti, iss, type, id}) => {
+                if (!jti || !iss || !type) {
+                    const key = id && `*${id || '*'}:${type || '*'}:${iss || '*'}` || `${jti || '*'}:${type || '*'}:${iss || '*'}`;
+                    return redisClient
+                        .keys(key)
+                        .then((keys) => (redisClient.del(...keys)));
+                }
+                return redisClient
+                    .del(`${jti}:${type}:${iss}`);
+            });
+    }
 }
 
 export default JWTService;
--- test/integration/JWTSerice.test.js
+++ test/integration/JWTSerice.test.js
@@ -15,9 +15,9 @@ chai.use(dirtyChai);
 
 
 describe('JWTService', () => {
-    const redisClient = new MockRedisClient();
-    const jwtService = new JWTService({redisClient});
     const secret = config.get('jwt.secret');
+    const redisClient = new MockRedisClient();
+    const jwtService = new JWTService({redisClient, secret});
 
     afterEach((done) => {
         redisClient
@@ -35,6 +35,33 @@ describe('JWTService', () => {
                     expect(decoded).to.have.property('jti');
                     expect(decoded).to.have.property('type', 'ACCESS');
                     expect(decoded).to.have.property('iss', 'garage');
+                    expect(decoded.jti).to.be.a('string');
+                    expect(decoded.type).to.be.a('string');
+                    expect(decoded.iss).to.be.a('string');
+                    expect(decoded.iat).to.be.a('number');
+                    done();
+                })
+                .catch(done)
+        });
+
+        it('', (done) => {
+            const id = shortId.generate();
+            const type = shortId.generate();
+            const iss = shortId.generate();
+            const sub = shortId.generate();
+            const secret = shortId.generate();
+            jwtService
+                .createToken({data: {id}, type, iss, sub}, secret)
+                .then((token) => (jwt.verify(token, secret)))
+                .then((decoded) => {
+                    console.log(decoded)
+                    expect(decoded).to.have.property('jti');
+                    expect(decoded).to.have.property('type', type);
+                    expect(decoded).to.have.property('iss', iss);
+                    expect(decoded).to.have.property('sub', sub);
+                    expect(decoded).to.have.property('data');
+                    expect(decoded.jti).to.have.string(`-${id}`);
+                    expect(decoded.data).to.eql({id});
                     expect(decoded.jti).to.be.a('string');
                     expect(decoded.type).to.be.a('string');
                     expect(decoded.iss).to.be.a('string');
@@ -74,16 +101,38 @@ describe('JWTService', () => {
             jwtService
                 .createToken({})
                 .then((token) => (jwt.verify(token, secret)))
-                .then((decoded) => (jwtService.isActualAccessToken({jti: decoded.jti})))
+                .then((decoded) => (jwtService.isActualAccessToken(decoded)))
                 .then((isActual) => {
                     expect(isActual).to.be.true();
                     done();
                 })
                 .catch(done)
-        })
+        });
+
+        it('', (done) => {
+            jwtService
+                .createToken({})
+                .then((token) => (jwtService.isActualAccessToken(token)))
+                .then((isActual) => {
+                    expect(isActual).to.be.true();
+                    done();
+                })
+                .catch(done)
+        });
+
+        it('', (done) => {
+            jwtService
+                .createToken({type: 'REFRESH'})
+                .then((token) => (jwtService.isActualAccessToken(token)))
+                .then((isActual) => {
+                    expect(isActual).to.be.false();
+                    done();
+                })
+                .catch(done)
+        });
     });
 
-    describe('#breakAccessToken', () => {
+    describe('#breakToken', () => {
         it('', (done) => {
             jwtService
                 .createToken({})
@@ -101,7 +150,70 @@ describe('JWTService', () => {
                             done();
                         })
                 })
+                .catch(done)
+        });
 
+        it('', (done) => {
+            jwtService
+                .createToken({})
+                .then((token) => {
+                    return jwtService
+                        .breakToken(token)
+                        .then((numberOfDeleteTokens) => {
+                            expect(numberOfDeleteTokens).to.be.a('number');
+                            expect(numberOfDeleteTokens).to.be.equals(1);
+                        })
+                        .then(() => (jwtService.isActualAccessToken(token)))
+                        .then((isActual) => {
+                            expect(isActual).to.be.false();
+                            done();
+                        })
+                })
+                .catch(done)
+        });
+
+        it.skip('', (done) => {
+            jwtService
+                .breakToken({jti:shortId.generate(), iss:shortId.generate(), type:shortId.generate()})
+                .then((numberOfDeleteTokens)=>{
+                    expect(numberOfDeleteTokens).to.be.a('number');
+                    expect(numberOfDeleteTokens).to.be.equals(0);
+                    done();
+                })
+                .catch(done)
+        });
+
+        it('', (done) => {
+            jwtService
+                .breakToken({jti: shortId.generate()})
+                .then((numberOfDeleteTokens) => {
+                    expect(numberOfDeleteTokens).to.be.a('number');
+                    expect(numberOfDeleteTokens).to.be.equals(0);
+                    done();
+                })
+                .catch(done)
+        });
+
+        it('', (done) => {
+            const id = shortId.generate();
+            Promise
+                .all([
+                    jwtService
+                        .createToken({id}),
+                    jwtService
+                        .createToken({id})
+                ])
+                .then(() => (jwtService.breakToken({id})))
+                .then((numberOfDeleteTokens) => {
+                    expect(numberOfDeleteTokens).to.be.a('number');
+                    expect(numberOfDeleteTokens).to.be.equals(2);
+                    return jwtService.getTokensPayload(id);
+                })
+                .then((payloads)=>{
+                    expect(payloads).to.be.a('array');
+                    expect(payloads).to.have.lengthOf(0);
+                    done();
+                })
                 .catch(done)
         })
     });
